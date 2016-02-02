351f97ad39d5bcbec8fdc0187e71c01304f0d086
cat_in_136 <cat.in.136+github@gmail.com>
2016-02-03 00:37:15 +0900

    add the example of "ClientSocket is initialized with name and socket and optional group" to spec_app.rb

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -8,7 +8,18 @@ describe ClientSocket do
     expect { ClientSocket.new(:name => nil, :socket => nil) }.to raise_error(ArgumentError)
     expect { ClientSocket.new(:name => "name", :socket => "bad") }.to raise_error(ArgumentError)
   end
-  # TODO how to initialize EventMachine::WebSocket::Connection as client_socket.socket
+
+  it "is initialized with name and socket and optional group" do
+    conn = EventMachine::WebSocket::Connection.new(nil, {})
+    socket = ClientSocket.new(:name => "name", :socket => conn)
+    expect(socket.name).to eq("name")
+    expect(socket.group).to eq([])
+    expect(socket.socket).to eq(conn)
+    socket = ClientSocket.new(:name => "name", :group => ["group"], :socket => conn)
+    expect(socket.name).to eq("name")
+    expect(socket.group).to eq(["group"])
+    expect(socket.socket).to eq(conn)
+  end
 end
 
 describe "/" do
