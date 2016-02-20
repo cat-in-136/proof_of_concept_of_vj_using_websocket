abfe4f178d8d177fe192ba4c1089a2eccdf5dd41
cat_in_136 <cat.in.136+github@gmail.com>
2016-02-20 21:53:04 +0900

    add client list to the controller view

--- public/controller.js
+++ public/controller.js
@@ -9,10 +9,34 @@ $(function(){
     ws.onmessage = function(event){
         var message_li = $('<li>').text(event.data);
         $("#msg-area").append(message_li);
+        
+        try {
+            var data = JSON.parse(event.data);
+            if ((data.type === "connected") ||
+                (data.type === "get_clients") ||
+                (data.type === "disconnected")) {
+                updateClientsList(data);
+            }
+        } catch (ex) {
+        }
     };
     
     ws.onclose = function(event){
         var message_li = $('<li>').text("CLOSED.");
         $("#msg-area").append(message_li);
     };
+
+    function updateClientsList(data) {
+        if (data.type === "get_clients") {
+            var list = [];
+            for (var i = 0; i < data.value.length; i++) {
+                var item = $("<li></li>");
+                item.text(data.value[i].name);
+                list.push(item);
+            }
+            $("#client-list").empty().append(list);
+        } else {
+            ws.send(JSON.stringify([{type: "get_clients"}]));
+        }
+    }
 });
